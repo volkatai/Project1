d62e483c2ed938a5b05d11f78adfa13f51846815
User <volkatai@yandex.ru>
2022-05-10 10:48:40 +0300

    5 commit

--- alice.py
+++ alice.py
@@ -31,23 +31,28 @@ def handle_dialog(res, req):
     user_id = req['session']['user_id']
     if req['session']['new']:
         res['response']['text'] = 'Привет! Я подскажу тебе на каком автобусе добраться до нужного места!'
+        res['response']['text'] = 'Введи начальную остановку.'
         return
     # Получаем города из нашего
     stops = get_stops(req)
-    # if not stops:
-    res['response']['text'] = f'Я готова построить маршрут от {stops}!'
-    # elif len(cities) == 1:
-    #     res['response']['text'] = 'Этот город в стране - ' + get_geo_info(cities[0], 'country')
-    # elif len(cities) == 2:
-    #     distance = get_distance(get_geo_info(cities[0], 'coordinates'), get_geo_info(cities[1], 'coordinates'))
-    #     res['response']['text'] = 'Расстояние между этими городами: ' + str(round(distance)) + ' км.'
+    if not stops:
+        res['response']['text'] = f'Я не поняла, повторите ещё раз.'
+    elif len(stops) == 1:
+        res['response']['text'] = 'Отлично! Теперь введите конечную остановку.'
+    elif len(stops) == 2:
+        start_stop = stops[0]
+        final_stop = stops[1]
+        # distance = get_distance(get_geo_info(cities[0], 'coordinates'), get_geo_info(cities[1], 'coordinates'))
+        res['response']['text'] = f'Я готова построить маршрут от остановки "{start_stop}" до остановки "{final_stop}"'
     #     res['response']['text'] = 'Расстояние между этими городами: ' + str(round(distance)) + ' км.'
     # else:
     #     res['response']['text'] = 'Слишком много городов!'
 
 
 def get_stops(req):
-    stops = req['request']['original_utterance']
+    stops = []
+    stop = req['request']['original_utterance']
+    stops.append(stop)
     return stops
 
 
